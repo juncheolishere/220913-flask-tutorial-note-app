d683ad0848606b527488ad6d854a847228914e68
juncheolishere <90208306+juncheolishere@users.noreply.github.com>
2022-09-13 20:28:43 +0900

    06 HTTP Request과 GET / POST

--- diary/auth.py
+++ diary/auth.py
@@ -1,18 +1,39 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 # Create Blueprint
 auth = Blueprint('auth', __name__)
 
 #route
-@auth.route('/sign-up')
-def sign_up():
-    return render_template('sign_up.html')
-
 @auth.route('/logout')
 def logout():
     return render_template('logout.html')
 
-# logub
-@auth.route('/sign-in')
+# login
+@auth.route('/sign-in', methods=['GET','POST'])
 def sign_in():
-    return render_template('sign_in.html', user="")
+    return render_template('sign_in.html')
+
+# sign-up
+@auth.route('/sign-up', methods=['GET','POST'])
+def sign_up():
+    if request.method == "POST":
+        # Split Data
+        email = request.form.get('email')
+        nickname = request.form.get('nickname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # 유효성 검사
+        if len(email) < 5 :
+            pass
+        elif len(nickname) < 2 :
+            pass
+        elif password1 == password2 :
+            pass
+        elif len(password1) < 7 :
+            pass
+        else:
+            # Create User -> DB
+            pass
+
+    return render_template('sign_up.html')
