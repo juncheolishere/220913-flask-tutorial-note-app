76960876141718db3a0a6e7e1d62e718db00c6c7
juncheolishere <90208306+juncheolishere@users.noreply.github.com>
2022-09-13 20:45:06 +0900

    07 메세지 플래싱

--- diary/auth.py
+++ diary/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 # Create Blueprint
 auth = Blueprint('auth', __name__)
@@ -25,15 +25,15 @@ def sign_up():
 
         # 유효성 검사
         if len(email) < 5 :
-            pass
+            flash('이메일은 5자 이상입니다.', category='error')
         elif len(nickname) < 2 :
-            pass
-        elif password1 == password2 :
-            pass
+            flash('닉네임은 2자 이상입니다..', category='error')
+        elif password1 != password2 :
+            flash('비밀번호가 서로 다릅니다.', category='error')
         elif len(password1) < 7 :
-            pass
+            flash('비밀번호가 너무 짧습니다.', category='error')
         else:
             # Create User -> DB
-            pass
+            flash('회원가입', category='success')
 
     return render_template('sign_up.html')
