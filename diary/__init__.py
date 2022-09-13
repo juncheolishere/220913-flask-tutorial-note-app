f57941e47cf4fd93a82c41b21c0adf6c5f923ac5
juncheolishere <90208306+juncheolishere@users.noreply.github.com>
2022-09-13 21:30:22 +0900

    08 DB연결 준비, SqlAlchemy

--- diary/__init__.py
+++ diary/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# Create DB Connection
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 #Create App
 def create_app():
     app = Flask(__name__)
-    # 앱에 사용될 시크릿 키 정의.
-    # 자동적으로 세팅이 된다. 로그인, 보안과 관련된 기능을 플라스크에서 행사할때
-    # 기준 값을 기준으로 행사되는데 그 기준되는 값을 설정하는 것이다.
-    # 어떠한 문자열이어도 상관없다. like boss.
     app.config['SECRET_KEY'] = 'jc_secret_key'
+    app.config['SQLAlchemy_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     # Import Blueprint
     from .views import views
