913b004b65bee131d792663ff5872a7d2fed9730
juncheolishere <90208306+juncheolishere@users.noreply.github.com>
2022-09-14 15:46:28 +0900

    11 데이터베이스 생성

--- diary/__init__.py
+++ diary/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 # Create DB Connection
 db = SQLAlchemy()
@@ -9,7 +10,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'jc_secret_key'
-    app.config['SQLAlchemy_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     # Import Blueprint
@@ -20,4 +21,16 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    # Create or Import Database
+    from .models import User, Note
+    create_database(app)
+
+
     return app
+
+# Create or Check Database file
+def create_database(app):
+    # check db file
+    if not path.exists(f'diary/{DB_NAME}'):
+        db.create_all(app=app)
+        print('>>>> create db')
--- diary/models.py
+++ diary/models.py
@@ -6,9 +6,10 @@ from datetime import datetime
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120))
-    nickname = db.Column(db.String(80), unique=True, nillable=False)
+    nickname = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200))
     notes = db.relationship('Note')
+    # 용이성을 위해서 설정된 notes
 
 # define Note Model
 class Note(db.Model):
@@ -16,4 +17,4 @@ class Note(db.Model):
     title = db.Column(db.String(50))
     content = db.Column(db.String(2000))
     datetime = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
